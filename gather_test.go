8dc49666ccd0ff89cde3d9205a1411b159847d1f
cameronjpr <cameronjpr@gmail.com>
2024-01-30 13:11:26 +0000

    add test for invalid path

--- gather_test.go
+++ gather_test.go
@@ -6,11 +6,20 @@ import (
 	"github.com/go-test/deep"
 )
 
-func TestGetAllKeys(t *testing.T) {
+func TestGoodPath(t *testing.T) {
 	keys := getAllKeys("./src/lang")
 	exp := map[string]string{"dashboard_actions_referrals_dialog_trigger_cta_description": "en/lang3.json", "key_1": "lang.json", "key_2": "lang.json", "key_3": "lang.json", "key_4": "lang2.json", "key_5": "lang2.json"}
 
 	if deep.Equal(keys, exp) != nil {
 		t.Errorf("getAllKeys('./src/lang') = %v; want %v", keys, exp)
+	}
+}
+
+func TestBadPath(t *testing.T) {
+	keys := getAllKeys("./foo")
+	exp := map[string]string{}
+
+	if deep.Equal(keys, exp) != nil {
+		t.Errorf("getAllKeys('./foo') = %v; want %v", keys, exp)
 	}
 }
