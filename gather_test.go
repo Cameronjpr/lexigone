1b5484afa25e7202cb6b3ad3a30041dbbfb953bb
cameronjpr <cameronjpr@gmail.com>
2024-01-31 08:33:33 +0000

    fix test

--- gather_test.go
+++ gather_test.go
@@ -8,7 +8,7 @@ import (
 
 func TestGoodPath(t *testing.T) {
 	keys := getAllKeys("./src/lang")
-	exp := map[string]string{"dashboard_actions_referrals_dialog_trigger_cta_description": "en/lang3.json", "key_1": "lang.json", "key_2": "lang.json", "key_3": "lang.json", "key_4": "lang2.json", "key_5": "lang2.json"}
+	exp := map[string]string{"dashboard_actions_referrals_dialog_trigger_cta_description": "./src/lang/en/lang3.json", "key_1": "./src/lang/lang.json", "key_2": "./src/lang/lang.json", "key_3": "./src/lang/lang.json", "key_4": "./src/lang/lang2.json", "key_5": "./src/lang/lang2.json"}
 
 	if deep.Equal(keys, exp) != nil {
 		t.Errorf("getAllKeys('./src/lang') = %v; want %v", keys, exp)
